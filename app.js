00884eac7de812caef287093a1a7aeacd0a90faf
juanpcarta <juanpcarta@gmail.com>
2021-09-08 17:41:32 -0300

    commit varios (login + register)

--- app.js
+++ app.js
@@ -12,3 +12,6 @@ app.listen(PORT, ()=>{ console.log('Servidor corriendo en http://localhost:4011/
 
 app.get('/', (req,res)=>{    res.sendFile(path.resolve("./views/home.html")); });
 
+app.get('/register', (req,res)=>{    res.sendFile(path.resolve("./views/register.html")); });
+
+app.get('/login', (req,res)=>{    res.sendFile(path.resolve("./views/login.html")); });
